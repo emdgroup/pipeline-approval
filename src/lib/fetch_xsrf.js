923e34e0a674a8b89827e4986029d4fd8e5682b4
Justin Nguyen <jnguyen@medacuitysoftware>
2018-09-12 16:49:16 -0400

    remove include in fetch request

--- src/lib/fetch_xsrf.js
+++ src/lib/fetch_xsrf.js
@@ -1,6 +1,5 @@
 export default function request(input, args = {}) {
   return fetch(input, Object.assign({
-    credentials: 'include',
     method: args.method || 'POST',
   })).then(res => res.json());
 }
