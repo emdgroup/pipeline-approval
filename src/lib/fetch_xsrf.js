b201f809f8b455131969c418199246ebe392b484
Justin Nguyen <jnguyen@medacuitysoftware>
2018-09-13 10:47:09 -0400

    enable cors

--- src/lib/fetch_xsrf.js
+++ src/lib/fetch_xsrf.js
@@ -1,6 +1,5 @@
 export default function request(input, args = {}) {
   return fetch(input, Object.assign({
     method: args.method || 'POST',
-    mode: 'no-cors',
   })).then(res => res.json());
 }
