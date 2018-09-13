94545f384bb3e9f254c34815fbd15719801529cf
Justin Nguyen <jnguyen@medacuitysoftware>
2018-09-13 14:34:49 -0400

    added access-control-allow-origin to fetch header

--- src/lib/fetch_xsrf.js
+++ src/lib/fetch_xsrf.js
@@ -1,5 +1,11 @@
 export default function request(input, args = {}) {
-  return fetch(input, Object.assign({
-    method: args.method || 'POST',
-  })).then(res => res.json());
+  return fetch(
+    input,
+    Object.assign({
+      method: args.method || 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    }),
+  ).then(res => res.json());
 }
