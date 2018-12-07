9af495de96a8e0627b2189b2860aba8ac30d90fa
Moritz Onken <onken@netcubed.de>
2018-12-07 20:01:29 +0100

    only show changed parameters

--- src/components/ParameterDiff.jsx
+++ src/components/ParameterDiff.jsx
@@ -1,65 +1,27 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { diffWords } from 'diff';
 import Table from './Table';
 
-const emptyValue = '-';
+const headers = ['Parameter', 'Value'];
 
-const ParameterDiffTableData = data => (
-  <Fragment>
-    {data.map(({ Name, Default, CurrentValue, NewValue }) => (
-      <tr key={Name}>
-        <td>{Name}</td>
-        <td>{Default}</td>
-        <td className="oldValue">{CurrentValue}</td>
-        <td className="newValue">{NewValue}</td>
-      </tr>
-    ))}
-  </Fragment>
-);
-
-class ParameterDiff extends Component {
-  static headers = ['Parameter', 'Default Value', 'Old Value', 'New Value'];
-
-  state = {
-    parameterDiffSet: null,
-  };
-
-  componentDidMount() {
-    this.reformat();
-  }
-
-  reformat = () => {
-    const { set } = this.props;
-    if (!this.isEmpty(set)) {
-      Object.keys(set).forEach((key) => {
-        set[key].Parameter = key;
-      });
-      this.setState({ parameterDiffSet: [set] });
-    } else {
-      this.setState({ parameterDiffSet: [] });
-    }
-  };
-
-  isEmpty = obj => Object.keys(obj).length === 0;
-
-  render() {
-    const { parameterDiffSet } = this.state;
-    return (
-      <Fragment>
-        <Table
-          headers={ParameterDiff.headers}
-          renderData={ParameterDiffTableData}
-          data={parameterDiffSet}
-          render
-        />
-      </Fragment>
-    );
-  }
+export default function ({ data }) {
+  return (
+    <Table headers={headers}>
+      <tbody>
+        {data
+          .filter(({ CurrentValue, NewValue }) => CurrentValue !== NewValue)
+          .map(({ Name, Default, CurrentValue, NewValue }) => (
+            <tr key={Name}>
+              <td className="font-weight-bold">{Name}</td>
+              <td className="diff">
+                {diffWords(CurrentValue, NewValue).map(({ added, removed, value }) => (
+                  <span className={added ? 'diff-add' : removed ? 'diff-remove' : 'diff-code-text'}>{value}</span>
+                ))}
+              </td>
+            </tr>
+          ))}
+      </tbody>
+    </Table>
+  );
 }
-
-ParameterDiff.propTypes = {
-  set: PropTypes.arrayOf(PropTypes.any).isRequired,
-};
-
-
-export default ParameterDiff;
