3eb6320d3b65ba9428203937d6208ddbf199a38c
Moritz Onken <onken@netcubed.de>
2019-02-22 09:42:25 -0500

    implement basic hash router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
-import React, { Component, Suspense } from 'react';
+import React, { useState, useEffect } from 'react';
 import PipelineChanges from './PipelineChanges';
 
-class App extends Component {
-  state = {};
+export default function () {
+  const [hash, setHash] = useState(document.location.hash);
+  useEffect(() => {
+    const cb = () => setHash(document.location.hash);
+    window.addEventListener('hashchange', cb);
+    return () => window.removeEventListener('hashchange', cb);
+  }, [document.location.hash]);
 
-  render() {
-    return (<PipelineChanges />);
-  }
+  return <PipelineChanges key={hash} />;
 }
-
-export default App;
